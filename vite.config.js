40946c07300ad5d903d939e655fdf07a2f9b716f
Sandra <sandra.simon-schmidt@dci-student.org>
2025-10-21 15:04:19 +0200

    Add server and preview host configurations
    Configure server and preview settings for Vite.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 8080
+  }
   base: '/Bundeslaender-game/', // <- wichtig für GitHub Pages
 });
