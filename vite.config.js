eeb371400269439625578bd2565ff6dc2c1f9c34
Sandra <sandra.simon-schmidt@dci-student.org>
2025-10-21 15:21:40 +0200

    Fix base path configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Bundeslaender-game/', // <- wichtig für GitHub Pages
   server: {
     host: true
   },
@@ -11,5 +12,5 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 8080
   }
-  base: '/Bundeslaender-game/', // <- wichtig für GitHub Pages
+  
 });
