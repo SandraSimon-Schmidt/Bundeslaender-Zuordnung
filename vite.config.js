2646b66c4e5529010cb3d698c985163e809d27c0
Sandra <sandra.simon-schmidt@dci-student.org>
2025-10-21 15:34:12 +0200

    Add allowedHosts to Vite preview configuration

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    port: 8080
+    port: 8080,
+    allowedHosts: ['localhost', 'bundeslaender-zuordnung.onrender.com']
   }
   
 });
